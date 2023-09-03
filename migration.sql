56e8bf5261302b966e4326b73c4dc630de053b1c
Anderson Júnior <115490626+AndersonJunior143@users.noreply.github.com>
2023-09-03 18:30:38 -0300

    Update migration.sql

--- migration.sql
+++ migration.sql
@@ -1,21 +1,4 @@
 -- CreateTable
-CREATE TABLE "User" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "email" TEXT NOT NULL,
-    "name" TEXT
-);
-
--- CreateTable
-CREATE TABLE "Post" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "title" TEXT NOT NULL,
-    "content" TEXT,
-    "published" BOOLEAN NOT NULL DEFAULT false,
-    "authorId" INTEGER NOT NULL,
-    CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
-);
-
--- CreateTable
 CREATE TABLE "Bebe" (
     "nomebebe" TEXT NOT NULL,
     "CPF" TEXT NOT NULL PRIMARY KEY,
@@ -45,8 +28,6 @@ CREATE TABLE "Medico" (
     "especialidade" TEXT NOT NULL
 );
 
--- CreateIndex
-CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Bebe_CPF_key" ON "Bebe"("CPF");
